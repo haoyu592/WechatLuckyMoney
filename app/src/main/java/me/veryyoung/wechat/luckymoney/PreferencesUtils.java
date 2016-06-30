658cb916fc273bc5f22ca111f6279ad490a0e92e
veryyoung <codingyoung@gmail.com>
2016-06-30 23:44:33 +0800

    Add ui

---
+++ app/src/main/java/me/veryyoung/wechat/luckymoney/PreferencesUtils.java
@@ -0,0 +1,34 @@
+package me.veryyoung.wechat.luckymoney;
+
+
+import de.robv.android.xposed.XSharedPreferences;
+
+public class PreferencesUtils {
+
+    private static XSharedPreferences instance = null;
+
+    private static XSharedPreferences getInstance() {
+        if (instance == null) {
+            instance = new XSharedPreferences(PreferencesUtils.class.getPackage().getName());
+            instance.makeWorldReadable();
+        } else {
+            instance.reload();
+        }
+        return instance;
+    }
+
+    public static boolean open() {
+        return getInstance().getBoolean("open", false);
+    }
+
+    public static boolean delay() {
+        return getInstance().getBoolean("delay", false);
+    }
+
+
+    public static int delayTime() {
+        return getInstance().getInt("delay_time", 0);
+    }
+}
+
+
